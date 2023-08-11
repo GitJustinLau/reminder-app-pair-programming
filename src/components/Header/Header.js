51a3dbda22a120d33b20cc2a5810b63fae9eb712
Justin Lau <justinlau094@hotmail.com>
2023-08-11 11:52:08 -0400

    style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import "./Header.scss"
 import logo from "../../assets/images/icons/reminder-icon.png"
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
+    
     return (
         <header className="header">
             <div className="header__logo"> 
@@ -11,7 +13,7 @@ const Header = () => {
             <div> 
                 <h1 className="header__title">Reminder</h1>
             </div>
-
+            {/* <Link to="/activity" className="header__link">Activities</Link> */}
         </header>
     );
 }
